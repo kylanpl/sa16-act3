b26a11f7ca8eeb3443e0abd2186d976759be621b
Kylan Plunk <kaplunk1@memphis.edu>
2024-04-30 02:53:46 -0500

    ruby app transferred

---
+++ pages/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def work(request):
+    return render(request, 'pages/work.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
